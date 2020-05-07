c00e15b90d03d21f2b0779b06c9561fabdd0590e
Chinedu Ojukwu <chineduoju@gmail.com>
2020-05-06 21:43:40 -0400

    order init

--- order.py
+++ order.py
@@ -28,7 +28,7 @@ class Order:
         GTC = 2
 
     def __init__(self, instrument: Instrument, type: Order.Type, time_in_force: Order.TimeInForce,
-                 quantity: int = None, price: float = None):
+                 quantity: int = None, price: float = None, stop_price: float = None):
         """Creates a new order.
 
         Args:
@@ -41,7 +41,13 @@ class Order:
         Returns:
             a new Order object.
         """
-        pass
+        self.instrument = instrument
+        self.type = type
+        self.time_in_force = time_in_force
+        self.quantity = quantity
+        self.price = price
+        self.stop_price = stop_price
+        self.id = None
 
     def __str__(self) -> str:
         """Custom pretty print function for an Order"""
@@ -66,7 +72,41 @@ class Order:
         Raises:
             MalformedOrderError: If the combinations of properties set on the order are not compatible.
         """
-        pass
+
+        switch (self.type) {
+            case BUY:  
+                    self.rh.place_buy_order(self.instrument.url, self.quantity, self.price) 
+                    break;
+            case SELL:  
+                    self.rh.place_sell_order(self.instrument.url, self.quantity, self.price) 
+                    break;
+            case LIMIT_BUY_ORDER:  
+                    self.rh.place_limit_buy_order(self.instrument.url, self.symbol, self.time_in_force, self.price, self.quantity) 
+                    break;
+            case LIMIT_SELL_ORDER:  
+                    self.rh.place_limit_sell_order(self.instrument.url, self.symbol, self.time_in_force, self.price, self.quantity) 
+                    break;
+            case MARKET_BUY_ORDER:  
+                    self.rh.place_market_buy_order(self.instrument.url, self.symbol, self.time_in_force, self.quantity) 
+                    break;
+            case MARKET_SELL_ORDER:  
+                    self.rh.place_market_sell_order(self.instrument.url, self.symbol, self.time_in_force, self.quantity) 
+                    break;
+            case STOP_LIMIT_BUY_ORDER:  
+                    self.rh.place_stop_limit_buy_order(self.instrument.url, self.symbol, self.time_in_force, self.price, self.stop_price, self.quantity) 
+                    break;
+            case STOP_LIMIT_SELL_ORDER:  
+                    self.rh.place_stop_limit_sell_order(self.instrument.url, self.symbol, self.time_in_force, self.price, self.stop_price, self.quantity) 
+                    break;
+            case STOP_LOSS_BUY_ORDER:  
+                    self.rh.place_stop_loss_buy_order(self.instrument.url, self.symbol, self.time_in_force, self.stop_price, self.quantity) 
+                    break;
+            case STOP_LOSS_SELL_ORDER: 
+                    self.rh.place_stop_loss_sell_order(self.instrument.url, self.symbol, self.time_in_force, self.stop_price, self.quantity) 
+                    break;
+            default: 
+                    raise(MalformedOrderError)
+        }
 
     def cancel(self) -> None:
         """Cancels the current order after it has been placed but not yet executed.
@@ -74,8 +114,10 @@ class Order:
         Raises:
             OrderCancellationError: If order has already been executed or terminated.
         """
+        self.rh.canel_order(order_id)
         pass
 
+    # TODO
     def status(self) -> OrderStatus:
         """Returns the status of the current order."""
         pass
